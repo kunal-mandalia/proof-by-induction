be69f493675d04a94fceb49bd801033f5f773973
Kunal Mandalia <k.mandalia@marketinvoice.com>
2017-11-29 19:03:08 +0000

    refactor: split proof process into functions

--- proof-by-induction.py
+++ proof-by-induction.py
@@ -2,27 +2,44 @@ from sympy import *
 x, y, z, k, n = symbols('x y z k n')
 init_printing(use_unicode=True)
 
-# Sum of all integers
-INPUT_FORMULA = 'n*(n+1)/2'
-INPUT_BASE_CASE = 1
-INPUT_NEXT_STEP = 'n+1'
+def verify_base_case(formula, valid_from_n, base_case):
+    computed_base_case = (sympify(formula)).subs(n, valid_from_n)
+    base_case_satisfied = computed_base_case == base_case
+    return base_case_satisfied
 
-# # Sum of all even numbers
-# INPUT_FORMULA = 'n*(n+1)'
-# INPUT_BASE_CASE = 2
-# INPUT_NEXT_STEP = '2*(n+1)'
+def expand_induction_hypothesis_k_plus_1(formula, next_expression):
+    # inductive hypothesis
+    inductive_step = formula.replace('n', 'k') + '+' + next_expression.replace('n', 'k')
+    return expand(inductive_step)
 
-# Prove base case f(1) = 1
-BASE_CASE = (sympify(INPUT_FORMULA)).subs(n, 1)
-print 'Base case met?', BASE_CASE,INPUT_BASE_CASE, BASE_CASE == INPUT_BASE_CASE
+def expand_formula_k_plus_1(formula):
+    return expand(formula.replace('n', '(k+1)'))
 
-# Prove if formula holds for k, it holds for k+1
-INDUCTION_1 = INPUT_FORMULA.replace('n', '(k+1)')
-EXPAND_INDUCTION_1 = expand(INDUCTION_1)
-print INDUCTION_1, EXPAND_INDUCTION_1
+def prove_arithmetic_series_formula(formula, valid_from_n, base_case, next_expression):
+    # verify base case
+    base_case_satisfied = verify_base_case(formula, valid_from_n, base_case)
 
-INDUCTION_2 = INPUT_FORMULA.replace('n', 'k') + '+' + INPUT_NEXT_STEP.replace('n', 'k')
-EXPAND_INDUCTION_2 = expand(INDUCTION_2)
-print INDUCTION_2, EXPAND_INDUCTION_2
+    # Inductive step
+    inductive_hypothesis = expand_induction_hypothesis_k_plus_1(formula, next_expression)
+    formula_next_term = expand_formula_k_plus_1(formula)
 
-print 'Proved?', EXPAND_INDUCTION_1 == EXPAND_INDUCTION_2
+    # If the inductive hypothesis matches the formula for the next term
+    return {
+        'base_case_satisfied': base_case_satisfied,
+        'inductive_hypothesis': inductive_hypothesis,
+        'formula_next_term': formula_next_term,
+        'proved': inductive_hypothesis == formula_next_term
+    }
+
+# Sum of all even numbers
+INPUT_FORMULA = 'n*(n+1)'
+INPUT_VALID_FROM_N = 1
+INPUT_BASE_CASE = 2
+INPUT_NEXT_STEP = '2*(n+1)'
+
+print prove_arithmetic_series_formula(
+    INPUT_FORMULA,
+    INPUT_VALID_FROM_N,
+    INPUT_BASE_CASE,
+    INPUT_NEXT_STEP
+    )
